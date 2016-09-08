04b2e1036ad68032818426182404e75eb3131ac2
Eugeniy Voronin <evoronin@weezlabs.com>
2016-09-08 21:00:18 +0300

    Axios config fix

--- src/app/http/configure.js
+++ src/app/http/configure.js
@@ -4,7 +4,6 @@ import { API_URL } from '../config';
 export default () => {
     $http.interceptors.request.use((config) => {
         config.url = `${API_URL}/${config.url}`;
-        config.headers = { 'Content-Type': 'application/json' };
         return config;
     });
 };
